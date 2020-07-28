2163a88c26e5034aacac26fcc8d23da0e74f32e4
Israel Torres <israeltorres@Israel-Mac-Mini.local>
2020-07-28 15:11:11 -0400

    Updates

---
+++ precache-manifest.8d24a7b6ed717ad0ccc0fab09ebcc5d7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de5737f4ab5162ff6526d78fd7db406",
+    "url": "/DR-Client-Review/index.html"
+  },
+  {
+    "revision": "122372400398cfdaa02a",
+    "url": "/DR-Client-Review/static/css/main.5eb85f64.chunk.css"
+  },
+  {
+    "revision": "150b009b50ccfde36262",
+    "url": "/DR-Client-Review/static/js/2.2694456e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DR-Client-Review/static/js/2.2694456e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122372400398cfdaa02a",
+    "url": "/DR-Client-Review/static/js/main.eeb980ef.chunk.js"
+  },
+  {
+    "revision": "47b97bc8dc8b619b107f",
+    "url": "/DR-Client-Review/static/js/runtime-main.267a9080.js"
+  },
+  {
+    "revision": "d545049f2fa0412aacdf8d56fd41c26e",
+    "url": "/DR-Client-Review/static/media/bg.d545049f.jpg"
+  },
+  {
+    "revision": "ebf905f0dc53afd2901df0183f7c5a6a",
+    "url": "/DR-Client-Review/static/media/blob1.ebf905f0.svg"
+  },
+  {
+    "revision": "39be387e38627d17024fe28817d06aea",
+    "url": "/DR-Client-Review/static/media/blob2.39be387e.svg"
+  },
+  {
+    "revision": "d15f77a1d0b52304de86b7236107b2d9",
+    "url": "/DR-Client-Review/static/media/blob3.d15f77a1.svg"
+  },
+  {
+    "revision": "a09c0225b2792113c61922191568bc02",
+    "url": "/DR-Client-Review/static/media/digital-name-dark.a09c0225.png"
+  },
+  {
+    "revision": "b1fba98786689688c7e94d23bfd8484d",
+    "url": "/DR-Client-Review/static/media/digital-name.b1fba987.png"
+  },
+  {
+    "revision": "f686e9f280e8f90477cda3fae9861b97",
+    "url": "/DR-Client-Review/static/media/dr_logo.f686e9f2.png"
+  },
+  {
+    "revision": "637fafd4e7e927a803771ca1c63e74ac",
+    "url": "/DR-Client-Review/static/media/image-end.637fafd4.png"
+  },
+  {
+    "revision": "8191249d9f401489a421b03d414f232a",
+    "url": "/DR-Client-Review/static/media/image-form1.8191249d.png"
+  },
+  {
+    "revision": "bdad2a574a15e1531d06f0417a5fae56",
+    "url": "/DR-Client-Review/static/media/image-form2.bdad2a57.png"
+  },
+  {
+    "revision": "4c89d07880f9bdee9f31d0be1a874bec",
+    "url": "/DR-Client-Review/static/media/image-form3.4c89d078.png"
+  },
+  {
+    "revision": "9609d58692416080eb821bf44e37854d",
+    "url": "/DR-Client-Review/static/media/image-form4.9609d586.png"
+  },
+  {
+    "revision": "debce40bde600380705c56ab2538d511",
+    "url": "/DR-Client-Review/static/media/image-form5.debce40b.png"
+  }
+]);
